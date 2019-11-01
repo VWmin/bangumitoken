7b8aa739520829b6ecbd580eda938b97b5868d54
Vfawsuki <fawsuki@gmail.com>
2019-11-01 23:26:38 +0800

    first commit

---
+++ src/main/java/com/vwmin/bangumitoken/BangumitokenApplication.java
@@ -0,0 +1,13 @@
+package com.vwmin.bangumitoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangumitokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BangumitokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vwmin/bangumitoken/BangumitokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vwmin.bangumitoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangumitokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
